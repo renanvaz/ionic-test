968e3972855a4f5b7b9efb71ee968f4285a7612d
Renan Vaz <renan.c.vaz@gmail.com>
2015-05-02 05:58:55 -0300

    Working...

---
+++ www/js/app.js
@@ -0,0 +1,79 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+// 'starter.controllers' is found in controllers.js
+
+var IS_WEB = typeof cordova === 'undefined' ? true : false;
+
+angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'templates/base.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.home', {
+    url: '/home',
+    views: {
+      menuContent: {
+        controller: 'HomeCtrl',
+        templateUrl: 'templates/home.html'
+      }
+    }
+  })
+
+  .state('app.events', {
+    url: '/events',
+    views: {
+      menuContent: {
+        controller: 'EventsCtrl',
+        templateUrl: 'templates/events.html'
+      }
+    }
+  })
+
+  .state('app.events-id', {
+    url: '/events/:id',
+    views: {
+      menuContent: {
+        controller: 'SearchCtrl',
+        templateUrl: 'templates/search.html'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/home');
+});
+
+if (IS_WEB) {
+  setTimeout(function(){
+    var domElement = document.querySelector('body');
+    angular.bootstrap(domElement, ['starter']);
+  }, 0);
+} else {
+  document.addEventListener('deviceready', function() {
+    var domElement = document.querySelector('body');
+    angular.bootstrap(domElement, ['starter']);
+  }, true);
+}
---
+++ www/js/controllers.js
@@ -0,0 +1,68 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope) {
+  ;
+})
+
+.controller('SearchCtrl', function($scope) {
+  $scope.events = [
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'}
+  ];
+})
+
+.controller('HomeCtrl', function($scope) {
+  $scope.nextEvents = [
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'}
+  ];
+})
+
+.controller('EventsCtrl', function($scope) {
+  $scope.events = [
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'},
+    {id: '1', title: 'CIRCUITO POPULAR DE CORRIDAS - PENHA', date: '01/05/2015', local: 'SÃO PAULO - SP / SP'}
+  ];
+});
